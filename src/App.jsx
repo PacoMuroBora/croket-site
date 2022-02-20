fd34745467322c22cca6392ce7df6adef5416b27
Paco Muro <paco@studioborabora.com>
2022-02-20 19:24:05 +0100

    noe_modules/.vite import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,44 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
+import './App.css';
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+
+const routes = [
+  {
+    exact: true,
+    path: '/',
+    component: () => <Home />,
+  },
+  {
+    exact: true,
+    path: '/about',
+    component: () => <About />,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <header className="App-header">{/* <Navbar /> */}</header>
+      <Router>
+        <Switch>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={route.path}
+                render={route.component}
+              />
+            );
+          })}
+        </Switch>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

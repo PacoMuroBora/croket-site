5ca57845835baeb9aa4451128973c43d65708d48
Paco Muro <paco@studioborabora.com>
2022-11-08 00:33:48 +0100

    add videos

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import { onMounted } from 'vue';
 
-const scrollRange = '1000';
+const scrollRange = '2000';
 
 onMounted(() => {
   gsap.fromTo(
@@ -23,14 +23,14 @@ onMounted(() => {
 
 <template>
   <div
-    class="bg-gradient-to-b from-indigo-900 to-slate-800 text-gray-100 relative"
+    class="bg-gradient-to-b from-fuchsia-900/20 to-black text-gray-100 relative"
     :style="{
-      height: 4 * scrollRange + 'px',
+      height: 3.5 * scrollRange + 'px',
     }"
   >
     <h1
       id="title"
-      class="z-10 font-bold italic text-5xl absolute inset-x-0 top-12 mx-8"
+      class="z-10 font-bold italic text-5xl sm:text-8xl absolute inset-x-0 top-12 mx-8"
     >
       STUDIO CROKET
     </h1>
@@ -56,8 +56,21 @@ onMounted(() => {
         height: 2 * scrollRange + 'px',
       }"
     ></div>
-    <div class="fixed inset-0">
-      <div class="w-full mx-auto">
+    <div
+      class="fixed inset-0 top-1/2 -translate-y-1/2 sm:top-0 sm:translate-y-0 flex justify-center items-center"
+    >
+      <div class="w-full mx-auto relative">
+        <div class="absolute inset-12 text-3xl sm:text-5xl">
+          <h1 class="font-bold italic mb-8">STUDIO CROKET</h1>
+          <div class="font-light">
+            Follow me on Instagram:
+            <br />
+            <a class="underline" href="https://www.instagram.com/pacocroket/"
+              >@pacocroket</a
+            >
+          </div>
+        </div>
+
         <VideoWrapper />
       </div>
     </div>
--- src/components/VideoWrapper.vue
+++ src/components/VideoWrapper.vue
@@ -1,30 +1,46 @@
 <template>
-  <div class="w-full aspect-video relative">
+  <div
+    class="w-[200%] pointer-events-none -translate-x-1/4 sm:w-full aspect-video relative sm:translate-x-0"
+  >
     <div id="mask0" class="absolute inset-0">
-      <video class="aspect-video w-full" autoplay muted loop>
-        <source src="assets/vid1.mp4" type="video/mp4" />
+      <video ref="vid1ref" class="aspect-video w-full" muted>
+        <source src="assets/videos/Redgitter.m4v" type="video/mp4" />
       </video>
     </div>
     <div id="mask1" class="absolute inset-0">
-      <video class="aspect-video w-full" autoplay muted loop>
-        <source src="assets/vid2.mp4" type="video/mp4" />
+      <video ref="vid2ref" class="aspect-video w-full" muted>
+        <source src="assets/videos/Lightning.m4v" type="video/mp4" />
       </video>
     </div>
     <div id="mask2" class="absolute inset-0">
-      <video class="aspect-video w-full" autoplay muted loop>
-        <source src="assets/vid3.mp4" type="video/mp4" />
+      <video ref="vid3ref" class="aspect-video w-full" muted>
+        <source src="assets/videos/Feedback.m4v" type="video/mp4" />
       </video>
     </div>
+    <div
+      class="absolute inset-0 transition-all"
+      :class="{ 'bg-black/50': !loaded }"
+    ></div>
   </div>
 </template>
 
 <script setup>
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 gsap.registerPlugin(ScrollTrigger);
 
+const vid1ref = ref();
+const vid2ref = ref();
+const vid3ref = ref();
+const loaded = ref(false);
+
 onMounted(() => {
+  setTimeout(() => {
+    loaded.value = true;
+    resetAllVideos();
+  }, 1000);
+
   gsap.to('#mask0', {
     clipPath: 'polygon(100% 0, 100% 0%, 100% 100%, 100% 100%)',
     scrollTrigger: {
@@ -67,7 +83,24 @@ onMounted(() => {
     .to('#mask2', {
       clipPath: 'polygon(100% 0, 100% 0%, 100% 100%, 100% 100%)',
     });
+
+  // restart all videos together
+  vid1ref.value.onended = () => {
+    resetAllVideos();
+  };
 });
+
+function resetAllVideos() {
+  resetVideo(vid1ref.value);
+  resetVideo(vid2ref.value);
+  resetVideo(vid3ref.value);
+}
+
+function resetVideo(videoEl) {
+  videoEl.pause();
+  videoEl.currentTime = 0;
+  videoEl.play();
+}
 </script>
 
 <style lang="scss" scoped>
